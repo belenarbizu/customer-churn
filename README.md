f7e4fd21fc611cfcd0eaa23912fc426a2d6fc798
belen-applymed <158149022+belen-applymed@users.noreply.github.com>
2026-01-22 18:32:57 +0100

    saved model

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 Las categorías numéricas no siguen una distribución normal
 Las categorías binarias con datos "Yes" y "No" fueron transformadas a 0/1
 Las categorías multi-clase fueron manejados con OneHotEncoder
+Baseline con LogisticRegression, pero RandomForest es mejor modelo para churn por el mejor recall y AUC
 No hay outliers
--- train.py
+++ train.py
@@ -1,9 +1,11 @@
+import os
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report, roc_auc_score
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import cross_val_score, RandomizedSearchCV
+import joblib
 
 
 param_grid = {
@@ -31,9 +33,7 @@ def create_model(X_train, y_train, X_test, y_test):
     cv = RandomizedSearchCV(RandomForestClassifier(random_state=42, class_weight='balanced'), 
                             param_grid, n_iter=10, cv=5, scoring='roc_auc', random_state=42, n_jobs=-1)
     cv.fit(X_train, y_train)
-    y_pred = cv.predict(X_test)
-    print("AUC:", cv.best_score_)
-    print(classification_report(y_test, y_pred))
+    return cv.best_estimator_
 
 
 def baseline_model(X_train, y_train, X_test, y_test):
@@ -45,12 +45,20 @@ def baseline_model(X_train, y_train, X_test, y_test):
     print("AUC:", roc_auc_score(y_test, y_proba))
 
 
+def save_model(model, file_path):
+    """Saves the model to a file."""
+    if not os.path.exists(os.path.dirname(file_path)):
+        os.makedirs(os.path.dirname(file_path))
+    joblib.dump(model, file_path)
+
+
 def main():
     file_path = 'data\\processed_data.csv'
     df = open_file(file_path)
     X_train, X_test, y_train, y_test = split_data(df)
     # baseline_model(X_train, y_train, X_test, y_test)
-    create_model(X_train, y_train, X_test, y_test)
+    best_model = create_model(X_train, y_train, X_test, y_test)
+    save_model(best_model, 'models\\model.pkl')
 
 
 if __name__ == "__main__":
